93bc122b15b7faf98af84a330e4cdae680ec0c82
theangelaluo <theangelaluo@gmail.com>
2020-07-10 00:49:30 -0700

    added text and buttons to main page

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "../App.css";
+import line from "./line.png"
 
 export default class Main extends Component {
 
@@ -9,8 +10,16 @@ export default class Main extends Component {
     return (
 
         <div className="background-img">
-        
-
+          <div className="main-section">
+            <h1 className="main-title">spectra 4.0</h1>
+            <div className="main-date">
+              <img src={line} alt={"horizontal white line"}/>
+              <h2 className="main-date-text">8/8-8/9/2020</h2>
+              <img src={line} alt={"horizontal white line"}/>
+            </div>
+            <a className="main-aref main-button main-button-hacker" href="https://tinyurl.com/sospectra">register to hack</a>
+            <a className="main-button main-button-mentor" href="https://tinyurl.com/spectramentors">register to mentor</a>
+          </div>
 
         </div>
 
