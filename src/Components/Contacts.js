b932b780f12d6f2aa06823fc07fd7cf1846dc7e0
theangelaluo <theangelaluo@gmail.com>
2020-07-12 14:12:56 -0700

    fix nav bug with contacts

--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Contact({ id }) {
   return (
-    <div className="contact">
+    <div className="contact" id={id}>
       <h1>Contact us</h1>
       <h2 className="contact-headline">Want to get involved or have any q's?</h2>
       <a className="main-aref main-button contact-button" href="mailto:hello@sospectra.com" target="_blank">
