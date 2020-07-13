54054b4033c0d38e35747ce0d8b65e205efcc007
samkitkat <samarbarakat23@gmail.com>
2020-07-12 23:26:58 -0700

    sizing

--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -4,7 +4,7 @@ export default function Contact({ id }) {
   return (
     <div className="contact contact-spacing" id={id}>
       <h1>Contact us</h1>
-      <h2 className="contact-headline">Want to get involved or have any q's?</h2>
+      <h2 className="contact-headline">Want to get involved or have any questions?</h2>
       <a className="main-aref main-button contact-button" href="mailto:hello@sospectra.com" target="_blank" rel="noopener noreferrer">
         Email us</a>
     </div>
