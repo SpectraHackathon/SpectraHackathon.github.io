4869b140f4db3f203b4dcf0e38201ec840605093
samkitkat <samarbarakat23@gmail.com>
2020-07-16 20:48:29 -0700

    sponsors

--- src/Components/MeetTheSponsors.js
+++ src/Components/MeetTheSponsors.js
@@ -1,4 +1,5 @@
 import React from "react";
+import braintree from "./sponsorLogos/braintree.png";
 
 
 export default function MeetTheSponsors({ title, subtitle, dark, id }) {
@@ -7,6 +8,8 @@ export default function MeetTheSponsors({ title, subtitle, dark, id }) {
       <div className="section-content" id={id}>
 
           <h1>{title}</h1>
+
+          <img src={braintree}/>
 
           <div className="row">
             {subtitle}
--- src/Components/sponsorsData.js
+++ src/Components/sponsorsData.js
@@ -1,19 +1,32 @@
+import braintree from "./sponsorLogos/braintree.png";
 import balsamiq from "./sponsorLogos/balsamiq.png";
 import hyperX from "./sponsorLogos/hyperX.png";
+import rstudio from "./sponsorLogos/rstudio.png";
 import echoAR from "./sponsorLogos/echoAR.png";
 import bugsee from "./sponsorLogos/bugsee.png";
+import fund from "./sponsorLogos/1517Fund.png";
+import benefit from "./sponsorLogos/benefit.jpeg";
+import clerky from "./sponsorLogos/clerky.svg";
+import imagi from "./sponsorLogos/imagilabs.png";
+
 
 const sponsorsData = [
     {
-        id: 3,
+        id: 2,
         img: balsamiq,
         name: "Balsamiq",
         size: "medium",
     },
     {
-        id: 4,
+        id: 3,
         img: hyperX,
         name: "HyperX",
+        size: "small",
+    },
+    {
+        id: 4,
+        img: rstudio,
+        name: "RStudio",
         size: "small",
     },
     {
@@ -26,6 +39,30 @@ const sponsorsData = [
         id: 6,
         img: bugsee,
         name: "Bugsee",
+        size: "xsmall",
+    },
+    {
+        id: 7,
+        img: fund,
+        name: "1517Fund",
+        size: "xsmall",
+    },
+    {
+        id: 8,
+        img: benefit,
+        name: "Benefit",
+        size: "xsmall",
+    },
+    {
+        id: 9,
+        img: clerky,
+        name: "Clerky",
+        size: "xsmall",
+    },
+    {
+        id: 10,
+        img: imagi,
+        name: "ImagiLabs",
         size: "xsmall",
     }
 ]
